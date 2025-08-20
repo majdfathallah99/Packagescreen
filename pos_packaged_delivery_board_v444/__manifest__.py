3649b3fdaef4edcbe21453de000277a7768bd1fe
majdfathallah99 <majdfathallah3@gmail.com>
2025-08-21 01:19:24 +0200

    Add files via upload

---
+++ pos_packaged_delivery_board_v444/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "POS Packaged Delivery Board",
+    "version": "18.0.3.1.0+pushnotify",
+    "summary": "Paid-only, live on validate. UI: no 'New' button (create disabled in views).",
+    "license": "LGPL-3",
+    "author": "You",
+    "category": "Point of Sale",
+    "depends": ['point_of_sale', 'product', 'mail'],
+    "data": [
+        "views/menu.xml",
+        "views/card_views.xml",
+        "data/server_actions.xml",
+        "security/ir.model.access.csv"
+    , "pos_packaged_delivery_board_v18_nowizard_paidonly_live_nocreate/data/server_actions.xml", "pos_packaged_delivery_board_v18_nowizard_paidonly_live_nocreate/data/alerts_channel.xml"],
+    "images": ["static/description/icon.svg"],
+    "installable": True,
+    "application": True
+}
