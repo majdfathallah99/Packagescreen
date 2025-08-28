48f647c941e950ea32119bca36d70d1d2cc593d6
majdfathallah99 <majdfathallah3@gmail.com>
2025-08-28 12:25:48 +0200

    Add files via upload

---
+++ Price_Check_Screen_v7_10/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Price Checker (Autoscan)",
+    "version": "18.0.1.2.0.9",
+    "summary": "Scan a barcode and instantly see product & price (no Enter)",
+    "author": "You",
+    "license": "LGPL-3",
+    "category": "Sales/Products",
+    "depends": ["product", "sale"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/price_checker_views.xml",
+        "views/price_checker_menu.xml"
+    ],
+    "assets": {
+        "web.assets_backend": [
+                        "Price_Check_Screen_v7_7/static/src/js/price_checker_autoscan.js"
+        ]
+    },
+    "application": True,
+    "images": ["static/description/icon.png"],
+    "icon": "Price_Check_Screen_v7_7/static/description/icon.png",
+    "installable": True
+}
