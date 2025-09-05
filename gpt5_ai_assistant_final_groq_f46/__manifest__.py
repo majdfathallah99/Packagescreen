29a76413e4be07225ce9abe9b52194dc2dbc9e2e
majdfathallah99 <majdfathallah3@gmail.com>
2025-09-05 11:06:43 -0700

    Add files via upload

---
+++ gpt5_ai_assistant_final_groq_f46/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'AI Assistant Live (Groq)',
+    'version': '1.0.0.post3',
+    'summary': 'Live voice chat assistant (Groq) with one-click app tile',
+    'author': 'Assistant',
+    'license': 'LGPL-3',
+    'depends': ['base', 'web'],
+    'data': ['views/menu_live.xml'],
+    'application': True,
+    'category': 'Productivity',
+    'images': ['static/description/icon.png'],
+}
